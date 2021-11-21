9c0524191adb589dee274239ed27690aea951dc5
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2021-11-21 22:01:36 +0300

    changed setup top level

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    py_modules = ['main',]
+    py_modules = ['canvas_roof',]
 )
 
