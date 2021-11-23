fcda7713b05610150529f85b2127c42753378ba4
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2021-11-23 11:05:16 +0300

    added install require setting insetup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,12 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    py_modules = ['canvas_roof',]
+    py_modules=['canvas_roof', ],
+    install_requires=[
+        'autopep8=1.5.7'
+        'canvas-roof-count=1.0'
+        'numpy=1.21.2'
+        'pycodestyle=2.7.0'
+        'toml=0.10.2'
+    ]
 )
